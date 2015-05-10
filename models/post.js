cc3cbed2bfad806fcb58e009bc3a702b193d3289
“mgoel1” <mgoel@walmartlabs.com>
2015-05-10 11:36:59 -0700

    adding create new post functionality

---
+++ models/post.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose')
+require('songbird')
+
+let postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  image: {
+    data: Buffer,
+    contentType: String
+  }
+})
+
+module.exports = mongoose.model('Post', postSchema)
--- routes.js
+++ routes.js
@@ -1,4 +1,8 @@
+let fs = require('fs')
+let multiparty = require('multiparty')
+let then = require('express-then')
 let isLoggedIn = require('./middleware/isLoggedIn')
+let Post = require('./models/post')
 
 module.exports = (app) => {
   let passport = app.passport
@@ -39,4 +43,33 @@ module.exports = (app) => {
     req.logout()
     res.redirect('/')
   })
+
+  app.get('/post/:postId?', (req, res) => {
+    let postId = req.params.postId
+    if (!postId) {
+      res.render('post.ejs', {
+        post: {},
+        verb: 'Create'
+      })
+    }
+  })
+
+// we are using then because we want it called only in case of error
+// if we use nodeifyit then it will always be called
+  app.post('/post/:postId?', then(async (req, res) => {
+    let postId = req.params.postId
+    if (!postId) {
+      let post = new Post()
+
+      // we will get a [field, file] array.. we can do [, files] and get the first file that we need
+      let [{title: [title], content: [content]},{image: [file]}] = await new multiparty.Form().promise.parse(req)
+      post.title = title
+      post.content = content
+      post.image.data = await fs.promise.readFile(file.path)
+      post.image.contentType = file.headers['content-type']
+      await post.save()
+      res.redirect('/blog/' + encodeURI(req.user.blogTitle))
+      return
+    }
+  }))
 }
